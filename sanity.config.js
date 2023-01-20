073bc29c53e1fc61fc95ca31910130c627ac009e
rennitbaby <work.kevin.ren@gmail.com>
2023-01-20 05:56:34 -0800

    testing CI, should install theme on studio

--- sanity.config.js
+++ sanity.config.js
@@ -1,4 +1,4 @@
-// import {theme} from 'https://themer.sanity.build/api/hues?default=darkest:15152e&primary=2276fc;50&transparent=8690a0;50&positive=43d675;300'
+import {theme} from 'https://themer.sanity.build/api/hues?default=darkest:15152e&primary=2276fc;50&transparent=8690a0;50&positive=43d675;300'
 
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
@@ -6,7 +6,7 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
-  // theme,
+  theme,
   name: 'default',
   title: 'CMS-Corporate-Sanity',
 
